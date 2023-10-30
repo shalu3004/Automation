2d5169a81db15b8d70b29bbc4fa47a27ae5422fc
shalu3004 <shalinu.saju@codinghands.in>
2023-10-30 10:26:00 +0530

    Whole project

---
+++ src/Test/One.java
@@ -0,0 +1,15 @@
+package Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class One {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\codin\\eclipse-workspace\\Automation\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://in.puma.com");
+        
+	}
+
+}
+
